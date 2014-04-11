8b7a8d299f26ee400775114c6ff0b61fa0042d96
Damian Kowal <damian@damiankowal.com>
2014-04-11 09:31:36 -0400

    Initial version using CCS animation to create the blinking effect

--- blink.js
+++ blink.js
@@ -1,27 +1,10 @@
 ;(function( $, window, undefined ) {
- 
-    $.fn.blink = function( speed ) {
 
+    $.fn.blink = function( speed ) {
+        
         // Set a default speed incase the argument passed is not valid
         speed = ( typeof speed === "number" && speed > 0 ) ? speed : 500;
 
-        function visibilityToggler( element ) {
-            // A function which takes as its argument a jQuery-wrapped element
-            // and returns a function which toggles that element's visibility
-
-            function toggleVisibility( index, visibility ) {
-                return ( visibility === "visible" ) ? "hidden" : "visible";
-            }
-
-            return function() {
-                element.css( "visibility", toggleVisibility );
-            };
-        }
-
-        window.setInterval( visibilityToggler( this ), speed );
-
-        return this; // Enable chaining
- 
+        return this.css( "animation", "blink " + ( speed / 1000 ) + "s infinite" );
     };
- 
 }( jQuery, window ));
